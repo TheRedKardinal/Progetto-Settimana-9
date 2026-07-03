1fed038e256f4fca36f0617170da38feecbfd90e
TheRedKardinal <claudio_giasi@icloud.com>
2026-07-03 16:37:27 +0200

    Fetch e render finiti

---
+++ src/assets/components/Gallery.jsx
@@ -0,0 +1,54 @@
+import { Component } from "react";
+import { Spinner } from "react-bootstrap";
+
+class Gallery extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: [],
+      loading: true,
+      error: null,
+    };
+  }
+
+  componentDidMount() {
+    const ApiFecth = async () => {
+      try {
+        const r = await fetch(
+          `http://www.omdbapi.com/?i=tt3896198&apikey=d8135f0f&s=${this.props.saga}`,
+        );
+        const dati = await r.json();
+        if (dati.Response === "True") {
+          this.setState({ movies: dati.Search });
+        } else {
+          this.setState({ error: dati.Error });
+        }
+      } catch (errore) {
+        console.error(`Errore di caricamento: ${errore}`);
+      } finally {
+        this.setState({ loading: false });
+      }
+    };
+    ApiFecth();
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <Spinner variant="danger" animation="border" />;
+    }
+
+    if (this.state.error) {
+      return <p>Errore: {this.state.error}</p>;
+    }
+
+    return (
+      <div>
+        {this.state.movies.map((film) => (
+          <img key={film.imdbID} src={film.Poster} alt={film.Title} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Gallery;
