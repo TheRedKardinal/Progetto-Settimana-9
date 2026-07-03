e0b1406bcbc41c84519595140306c46d1b1dc9e0
TheRedKardinal <claudio_giasi@icloud.com>
2026-07-03 12:04:30 +0200

    Primo commit con: creazione della repo react per git, importazione di logo tagliato di troppi bordi e ridimensionato da me, importazione di navbar e stilizzazione di quest'ultima e installazione delle bootstrap-icons con relativo inserimento nel nav

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import NavbarComp from "./assets/components/NavbarComp";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "./App.css";
+
+function App() {
+  return (
+    <header>
+      <NavbarComp />
+    </header>
+  );
+}
+
+export default App;
---
+++ src/assets/components/NavbarComp.jsx
@@ -0,0 +1,37 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import logo from "../img/netflix-logo.png";
+
+function NavbarComp() {
+  return (
+    <Navbar className="navbar" data-bs-theme="dark">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img src={logo} alt="Netflix logo image" />
+        </Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">TV Shows</Nav.Link>
+          <Nav.Link href="#pricing">Movies</Nav.Link>
+          <Nav.Link href="#pricing">Recently Added</Nav.Link>
+          <Nav.Link href="#pricing">My List</Nav.Link>
+        </Nav>
+        <Nav className="ms-auto">
+          <Nav.Link className="navbar-icons">
+            <i className="bi bi-search"></i>
+          </Nav.Link>
+          <Nav.Link>KIDS</Nav.Link>
+          <Nav.Link className="navbar-icons">
+            <i className="bi bi-bell-fill"></i>
+          </Nav.Link>
+          <Nav.Link className="navbar-icons">
+            <i className="bi bi-person-fill"></i>
+          </Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavbarComp;
